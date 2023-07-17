af84d6e4d55b72a5ab3b47ef4dbe4cf9276d1015
David Crespo <david.crespo@oxidecomputer.com>
2023-07-17 14:43:32 -0500

    ok put that back

--- static/http-client.test.ts
+++ static/http-client.test.ts
@@ -1,3 +1,5 @@
+// @vitest-environment happy-dom
+
 /**
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
