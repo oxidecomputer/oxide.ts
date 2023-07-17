435b4d81e3f8841b5e6d2570ee358c6f8ceb7cc1
Justin Bennett <git@just-be.dev>
2023-07-17 12:25:41 -0400

    Add more headers

--- tools/gen.sh
+++ tools/gen.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, you can obtain one at https://mozilla.org/MPL/2.0/.
+
 set -o errexit
 set -o pipefail
 
