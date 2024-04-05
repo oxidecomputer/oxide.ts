4e27c28f0768677f71da45bffe47253ed365989a
David Crespo <david.crespo@oxidecomputer.com>
2024-04-05 11:20:37 -0500

    handle array response even though we shouldn't have them

--- client/Api.ts
+++ client/Api.ts
@@ -6221,7 +6221,7 @@ export class Api extends HttpClient {
      * Get BFD status
      */
     networkingBfdStatus: (_: EmptyObj, params: FetchParams = {}) => {
-      return this.request<void>({
+      return this.request<BfdStatus[]>({
         path: `/v1/system/networking/bfd-status`,
         method: "GET",
         ...params,
@@ -6276,7 +6276,7 @@ export class Api extends HttpClient {
       { query }: { query?: NetworkingBgpAnnounceSetListQueryParams },
       params: FetchParams = {}
     ) => {
-      return this.request<void>({
+      return this.request<BgpAnnouncement[]>({
         path: `/v1/system/networking/bgp-announce`,
         method: "GET",
         query,
@@ -6318,7 +6318,7 @@ export class Api extends HttpClient {
       { query }: { query?: NetworkingBgpImportedRoutesIpv4QueryParams },
       params: FetchParams = {}
     ) => {
-      return this.request<void>({
+      return this.request<BgpImportedRouteIpv4[]>({
         path: `/v1/system/networking/bgp-routes-ipv4`,
         method: "GET",
         query,
@@ -6329,7 +6329,7 @@ export class Api extends HttpClient {
      * Get BGP peer status
      */
     networkingBgpStatus: (_: EmptyObj, params: FetchParams = {}) => {
-      return this.request<void>({
+      return this.request<BgpPeerStatus[]>({
         path: `/v1/system/networking/bgp-status`,
         method: "GET",
         ...params,
--- client/msw-handlers.ts
+++ client/msw-handlers.ts
@@ -885,7 +885,7 @@ export interface MSWHandlers {
   networkingBfdStatus: (params: {
     req: Request;
     cookies: Record<string, string>;
-  }) => Promisable<StatusCode>;
+  }) => Promisable<HandlerResult<Api.BfdStatus[]>>;
   /** `GET /v1/system/networking/bgp` */
   networkingBgpConfigList: (params: {
     query: Api.NetworkingBgpConfigListQueryParams;
@@ -909,7 +909,7 @@ export interface MSWHandlers {
     query: Api.NetworkingBgpAnnounceSetListQueryParams;
     req: Request;
     cookies: Record<string, string>;
-  }) => Promisable<StatusCode>;
+  }) => Promisable<HandlerResult<Api.BgpAnnouncement[]>>;
   /** `POST /v1/system/networking/bgp-announce` */
   networkingBgpAnnounceSetCreate: (params: {
     body: Json<Api.BgpAnnounceSetCreate>;
@@ -927,12 +927,12 @@ export interface MSWHandlers {
     query: Api.NetworkingBgpImportedRoutesIpv4QueryParams;
     req: Request;
     cookies: Record<string, string>;
-  }) => Promisable<StatusCode>;
+  }) => Promisable<HandlerResult<Api.BgpImportedRouteIpv4[]>>;
   /** `GET /v1/system/networking/bgp-status` */
   networkingBgpStatus: (params: {
     req: Request;
     cookies: Record<string, string>;
-  }) => Promisable<StatusCode>;
+  }) => Promisable<HandlerResult<Api.BgpPeerStatus[]>>;
   /** `GET /v1/system/networking/loopback-address` */
   networkingLoopbackAddressList: (params: {
     query: Api.NetworkingLoopbackAddressListQueryParams;
