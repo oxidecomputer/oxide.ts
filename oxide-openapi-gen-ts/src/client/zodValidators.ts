15dea56f454db58ad810af74453d21d864f3340f
David Crespo <david.crespo@oxidecomputer.com>
2025-07-28 11:46:25 -0500

    remove license block from other output files

--- oxide-openapi-gen-ts/src/client/zodValidators.ts
+++ oxide-openapi-gen-ts/src/client/zodValidators.ts
@@ -29,14 +29,6 @@ export function generateZodValidators(
 
   w(`/* eslint-disable */
 
-  /**
-   * This Source Code Form is subject to the terms of the Mozilla Public
-   * License, v. 2.0. If a copy of the MPL was not distributed with this
-   * file, you can obtain one at https://mozilla.org/MPL/2.0/.
-   *
-   * Copyright Oxide Computer Company
-   */
-
   import { z, ZodType } from 'zod';
   import { processResponseBody, uniqueItems } from './util';
 
